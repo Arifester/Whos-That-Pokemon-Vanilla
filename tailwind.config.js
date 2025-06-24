d4e1a8c94176cd56d1b07931d6cd58e3572926cb
Arifester <trainersterarif@gmail.com>
2025-06-24 22:07:04 +0700

    fix: update README and HTML to remove unnecessary hidden classes; enhance game over and Pokedex screen visibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ export default {
   content: [
     "./index.html", // Memindai file HTML utama
     "./src/**/*.{js,ts,jsx,tsx}", // Memindai semua file JS di dalam folder src
-    "./main.js" // Memindai file main.js
   ],
   theme: {
     extend: {},
