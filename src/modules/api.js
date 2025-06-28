12e5b961f037119077a3359bdc1faa8f3014b2cc
Arifester <trainersterarif@gmail.com>
2025-06-28 20:27:56 +0700

    fix: update GEN_RANGES to include all Pokémon generations

--- src/modules/api.js
+++ src/modules/api.js
@@ -6,6 +6,11 @@ const GEN_RANGES = {
   gen2: { offset: 151, limit: 100 },
   gen3: { offset: 251, limit: 135 },
   gen4: { offset: 386, limit: 107 },
+  gen5: { offset: 493, limit: 156 },
+  gen6: { offset: 649, limit: 72 },
+  gen7: { offset: 721, limit: 88 },
+  gen8: { offset: 809, limit: 96 },
+  gen9: { offset: 905, limit: 120 },
 };
 
 function getValidPokemonIds(generations) {
